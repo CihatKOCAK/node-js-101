4ac14e45e649f88f4f0faf804a9ba3f67804b9be
Cihat KOCAK <pcihatkocakks@gmail.com>
2023-02-05 01:21:13 +0300

    weatherAppOK

--- weather-project/app.js
+++ weather-project/app.js
@@ -1,27 +1,49 @@
 const express = require("express");
-
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  const query = req.body.cityName;
+  const apiKey = "cf0ad2d2ec86893c0bb720d2e71677b5";
+  const unit = "metric";
+
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=London&appid=cf0ad2d2ec86893c0bb720d2e71677b5";
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apiKey +
+    "&units=" +
+    unit;
   https.get(url, (response) => {
     console.log(response.statusCode);
 
     response.on("data", (data) => {
-        const weatherData = JSON.parse(data);
-        const temp = weatherData.main.temp;
-        const weatherDescription = weatherData.weather[0].description;
-        const icon = weatherData.weather[0].icon;
-        console.log(temp);
-        console.log(weatherDescription);
-        console.log(icon);
-        });
-  });
+      const weatherData = JSON.parse(data);
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      console.log(weatherDescription);
+      console.log(icon);
 
-  res.send("Server up and running!");
+      res.send(
+        "<h1>The temperature in " +
+          query +
+          " is " +
+          " degrees Celcius.</h1> <p>The weather is currently " +
+          weatherDescription +
+          "</p> <img src='http://openweathermap.org/img/w/" +
+          icon +
+          ".png'>"
+      );
+    });
+  });
 });
 
 app.listen(3000, () => {
